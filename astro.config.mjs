2bd49da85811efac12bcd470ae1f8be57e377180
Malo Germond <github@matchalo.org>
2025-12-30 15:40:52 +0100

    :pencil2: Glob issue due to handleLocales

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,6 @@ import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import tailwindcss from '@tailwindcss/vite';
 
-import sitemap from '@astrojs/sitemap';
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact(), tailwindcss()],
--- src/utils/handleLocales.js
+++ src/utils/handleLocales.js
@@ -1,9 +1,10 @@
 // src/utils/translations.js
-const projects = import.meta.glob('../locales/*.json', { eager: true });
+const projects = import.meta.glob('/src/locales/*.json', { eager: true });
 
 export function getTranslation(lang, name) {
   const key = name
-    ? '../locales/' + name + '_' + lang + '.json'
-    : '../locales/' + lang + '.json';
-  return projects[key] || {};
+    ? '/src/locales/' + name + '_' + lang + '.json'
+    : '/src/locales/' + lang + '.json';
+
+  return projects[key];
 }
