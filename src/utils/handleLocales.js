937ea81c7fd2d5b7bc402903adf8fff15549f4df
Malo Germond <github@matchalo.org>
2025-12-30 16:42:28 +0100

    :bug: Did I fix by removing octal characters

--- src/utils/handleLocales.js
+++ src/utils/handleLocales.js
@@ -2,8 +2,6 @@
 const locales = import.meta.glob('/src/locales/*.json', { eager: true });
 
 export function getTranslation(lang, name) {
-  console.log('get translation');
-  console.log({ locales }, { lang }, { name });
   const key = name
     ? '/src/locales/' + name + '_' + lang + '.json'
     : '/src/locales/' + lang + '.json';
