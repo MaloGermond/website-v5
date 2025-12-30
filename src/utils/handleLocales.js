7b82dc8c25ce3955000acb9db05bcec6dbe56abf
Malo Germond <github@matchalo.org>
2025-12-30 16:33:56 +0100

    :goal_net: Trying to understang where issue come from

--- src/utils/handleLocales.js
+++ src/utils/handleLocales.js
@@ -1,5 +1,8 @@
 // src/utils/translations.js
 const projects = import.meta.glob('/src/locales/*.json', { eager: true });
+const loc = import.meta.glob('../locales/*.json', { eager: true });
+console.log({ projects });
+console.log({ loc });
 
 export function getTranslation(lang, name) {
   const key = name
